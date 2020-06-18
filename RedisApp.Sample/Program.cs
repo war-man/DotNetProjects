bdc19f02108543d98d746b2695f14a516f477104
aaron <969340992@qq.com>
2020-06-18 09:53:47 +0800

    添加项目文件。

---
+++ RedisApp.Sample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisApp.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("End....");
+
+            var redisHelper = new RedisHelper();
+
+            var key = Guid.NewGuid().ToString();
+
+            //var s = redisHelper.StringSet(key, "123");
+            //Console.WriteLine(s ? "OK" : "No");
+
+            var token = Environment.MachineName;
+            var s = redisHelper.LockTake(key, token, TimeSpan.FromMinutes(1));
+            if (s)
+            {
+                try
+                {
+                    Console.WriteLine("Working..........");
+                }
+                finally
+                {
+                    redisHelper.LockRelease(key, token);
+                }
+            }
+
+            Console.WriteLine("End....");
+            Console.ReadKey();
+        }
+    }
+}
