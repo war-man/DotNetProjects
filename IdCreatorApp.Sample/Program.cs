f53e2489c96f7f840318bb3721a2a353a2d9a564
aaron <969340992@qq.com>
2020-06-28 16:45:11 +0800

    add

---
+++ IdCreatorApp.Sample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdCreatorApp.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IdCreator c = new IdCreator(0, 16);
+            var id = c.Create();
+
+            Console.WriteLine(id);
+            Console.ReadKey();
+        }
+    }
+}
