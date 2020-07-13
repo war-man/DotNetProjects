62b72f870cb4c3290442adff345a088409ec9b62
aaron <969340992@qq.com>
2020-07-13 09:43:29 +0800

    add mencached

---
+++ MemcachedApp.Sample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemcachedApp.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MemcachedHelper memcachedHelper = new MemcachedHelper();
+
+                var key = Guid.NewGuid().ToString();
+                var set = memcachedHelper.Insert(key, "abc", 1);
+                var get = memcachedHelper.Get(key);
+                Console.WriteLine(get);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
