108c99dd433db9933936205bd175c8a469ffa332
aaron <969340992@qq.com>
2020-08-04 15:02:45 +0800

    update

--- JwtApp.Sample/Controllers/UserController.cs
+++ JwtApp.Sample/Controllers/UserController.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 
 namespace JwtApp.Sample.Controllers
 {
